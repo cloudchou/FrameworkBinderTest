135ca6c0b2e2536ea4cfe47313db2e78c7893bbf
Cloud Chou <yaoyun229@gmail.com>
2014-04-17 17:22:56 +0800

    初始化

---
+++ BServer/Android.mk
@@ -0,0 +1,19 @@
+# Copyright 2008 The Android Open Source Project
+#
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_MODULE := bserver 
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_JAVA_LIBRARY)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := bserver
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/bin
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_SRC_FILES := bserver
+include $(BUILD_PREBUILT)
+
